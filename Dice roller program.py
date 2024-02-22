e4afe7b290d3f81b90b68399531e6e7176f85696
Putta Saikiran <119027987+saikiran-putta@users.noreply.github.com>
2024-02-22 12:45:45 +0530

    Add files via upload

---
+++ Dice roller program.py
@@ -0,0 +1,66 @@
+
+#print("\u25CF \u250C \u2500 \u2510 \u2502 \u2514 \u2518")
+#● ┌ ─ ┐ │ └ ┘
+import random
+
+dice_art = {
+    1:    ( "┌──────────┐",
+            "│          │",
+            "│    ●     │",
+            "│          │",
+            "└──────────┘"),
+
+    2:    ( "┌──────────┐",
+            "│  ●       │",
+            "│          │",
+            "│       ●  │",
+            "└──────────┘"),
+
+    3:    ( "┌──────────┐",
+            "│  ●       │",
+            "│     ●    │",
+            "│        ● │",
+            "└──────────┘"),
+
+    4:    ( "┌──────────┐",
+            "│ ●     ●  │",
+            "│          │",
+            "│ ●     ●  │",
+            "└──────────┘"),
+
+    5:    ( "┌──────────┐",
+            "│ ●     ●  │",
+            "│    ●     │",
+            "│ ●     ●  │",
+            "└──────────┘"),
+
+    6:     ("┌──────────┐",
+            "│  ●  ●  ● │",
+            "│          │",
+            "│  ●  ●  ● │",
+            "└──────────┘")
+}
+
+
+dice = []
+total = 0
+num_of_dice = int(input("How many dice?: "))
+for die in range(num_of_dice):
+    dice.append(random.randint(1,6))
+print(dice)
+#for die in range(num_of_dice):
+#   for line in dice_art.get(dice[die]):
+#        print(line)
+
+
+for line in range(5):
+    for die in dice:
+        print(dice_art.get(die)[line], end = "")
+    print()
+
+
+
+for die in dice:
+    total += die
+print(f"your total is: {total}")
+
